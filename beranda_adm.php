258f1129858c396f41bc826002d57bf7e89c86a6
developerTFL <77961136+developerTFL@users.noreply.github.com>
2021-01-25 16:21:17 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,39 @@
+
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert beranda">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Sistem Informasi Pencatatan Surat Masuk dan Surat Keluar PT. BSP Bunut Rubber Factory</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/bsp.jpg"></img></p>
+                          <p class="label-title" align="center"><strong>PT. BSP BUNUT RUBBER FACTORY</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Pencatatan Surat Masuk dan Surat Keluar PT. BSP Bunut Rubber Factory.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan pencatatan data surat dan jumlah surat.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan pencatatan data surat dan jumlah surat.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead>
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
---
+++ beranda_user.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Sistem Informasi Pencatatan Surat Masuk dan Surat Keluar PT. BSP Bunut Rubber Factory</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/bsp.jpg"></img></p>
+                          <p class="label-title" align="center"><strong>PT. BSP BUNUT RUBBER FACTORY</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman User Sistem Informasi Pencatatan Surat Masuk dan Surat Keluar PT. BSP Bunut Rubber Factory.<br>
+                            Melalui halaman ini user hanya dapat melihat pencatatan data surat dan jumlah surat.<br><br>
+                            Akses menu Laporan pada bagian atas sistem untuk melihat pencatatan data surat dan jumlah surat keluar.<br>
+                            <br><br><br>
+
+                          </p>
+
+                         </thead>
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
